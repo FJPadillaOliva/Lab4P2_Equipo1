5180efe6d5f896dd8f2649ba8e4f7ec6f9b2b7e7
Lloyd Cooper <cooperc913@gmail.com>
2023-05-12 14:15:23 -0600

    commit clases

--- src/lab4p2_equipo1/Ingenieros.java
+++ src/lab4p2_equipo1/Ingenieros.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4p2_equipo1;
 
-/**
- *
- * @author CUSTOMER PC
- */
 public class Ingenieros {
+    private int numeroRRHH;
+    private String nombreUsuario;
+    private String contraseña;
+    
+    
     
 }
--- src/lab4p2_equipo1/SeresVivos.java
+++ src/lab4p2_equipo1/SeresVivos.java
@@ -1,5 +1,9 @@
 package lab4p2_equipo1;
 
 public class SeresVivos {
-    
+    private String nombre;
+    private String grupoSanguineo;
+    private String sexo;
+    private double altura;
+    private double peso;
 }
