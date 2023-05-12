256911660de0f0aef88f3229ef46b96808757034
FJPadillaOliva <fjpo@unitec.edu>
2023-05-12 16:12:56 -0600

    Menu main

--- src/lab4p2_equipo1/Lab4P2_Equipo1.java
+++ src/lab4p2_equipo1/Lab4P2_Equipo1.java
@@ -20,47 +20,123 @@ public class Lab4P2_Equipo1 {
                     + "8.Modificar un medio de transporte\n"
                     + "9.Eliminar un medio de transporte\n"
                     + "10.Viaje\n"
-                    + "11.Salir");
+                    + "11.Imprimir medios de transporte\n"
+                    + "12.Salir");
             opcion = read.nextInt();
             switch(opcion){
                 case 1:
-                    
+                    int tipo;
+                    System.out.println("Ingrese el tipo de ser vivo a agregar(1-Ingeniero/2-Primate): ");
+                    tipo =  read.nextInt();
+                    switch (tipo) {
+                        case 1:
+                            System.out.println("Ingrese el numero RRHH: ");
+                            int numRRHH = read.nextInt();
+                            System.out.println("Ingrese el nombre de usuario: ");
+                            read.nextLine();
+                            String nombreUsuario= read.nextLine();
+                            System.out.println("Ingrese la contraseña: ");
+                            String contraseña = read.nextLine();
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el metodo de transporte asignado: ");
+                            
+                            System.out.println("Ingrese la cantidad de comida: );");
+                            double cantComida = read.nextDouble();
+                            System.out.println("Ingrese la cantidad de comida por Km: ");
+                            double cantComidaKm = read.nextDouble();
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");;
+                    }
                     break;
                 case 2:
-                    
+                    System.out.println("Ingrese el tipo de ser vivo a modificar(1-Ingeniero/2-Primate): ");
+                    tipo =  read.nextInt();
+                    switch (tipo) {
+                        case 1:
+                            System.out.println("Ingrese el numero RRHH: ");
+                            int numRRHH = read.nextInt();
+                            System.out.println("Ingrese el nombre de usuario: ");
+                            read.nextLine();
+                            String nombreUsuario= read.nextLine();
+                            System.out.println("Ingrese la contraseña: ");
+                            String contraseña = read.nextLine();
+                            break;
+                        case 2:
+                            System.out.println("Ingrese el metodo de transporte asignado: ");
+                            
+                            System.out.println("Ingrese la cantidad de comida: );");
+                            double cantComida = read.nextDouble();
+                            System.out.println("Ingrese la cantidad de comida por Km: ");
+                            double cantComidaKm = read.nextDouble();
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");;
+                    }
                     break;
                 case 3:
-                    
+                    System.out.println("Ingrese el tipo de ser vivo a eliminar(1-Ingeniero/2-Primate): ");
+                    tipo =  read.nextInt();
+                    switch (tipo) {
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");
+                            break;
+                    }
                     break;
                 case 4:
-                    
+                    System.out.println("Ingrese el nombre del planeta: ");
+                    String nombre = read.next();
+                    System.out.println("Ingrese la distancia entre la tierra y el planeta: ");
+                    int distancia = read.nextInt();
                     break;
                 case 5:
-                    
+                    System.out.println("Ingrese el nombre del planeta a modificar: ");
+                    String nombreN = read.next();
+                    System.out.println("Ingrese el nombre del planeta: ");
+                    nombre = read.next();
+                    System.out.println("Ingrese la distancia entre la tierra y el planeta: ");
+                    distancia = read.nextInt();
                     break;
                 case 6:
-                    
+                    System.out.println("Ingrese el nombre del planeta a eliminar: ");
+                    nombreN = read.next();
                     break;
                 case 7:
-                    
+                    System.out.println("Ingrese la cantidad de porcentaje a la que esta lleno el tanque: ");
+                    int cantTanque = read.nextInt();
+                    System.out.println("Ingrese la cantidad de Km que puede recorrer un tanque lleno: ");
+                    int kmsTanque = read.nextInt();
                     break;
                 case 8:
-                    
+                    System.out.println("Ingrese la nueva cantidad de porcentaje a la que esta lleno el tanque: ");
+                    int cantTanqueN = read.nextInt();
+                    System.out.println("Ingrese la nueva cantidad de Km que puede recorrer un tanque lleno: ");
+                    int kmsTanqueN = read.nextInt();
                     break;
                 case 9:
-                    
+                    System.out.println("");
                     break;
                 case 10:
                     
                     break;
                 case 11:
-                    opcion = 11;
+                    
+                    break;
+                case 12:
+                    opcion = 12;
                     break;
                 default:
                     System.out.println("Opcion no valida");
                     break;
             }
-        } while (opcion != 11);
+        } while (opcion != 12);
     }
     
 }
